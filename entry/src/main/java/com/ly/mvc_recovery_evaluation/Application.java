30b5d89226da97e58ba3f55349aa8a3d243d46df
liuyue <liuyue_yq@kedacom.com>
2022-04-07 15:44:39 +0800

    init

---
+++ entry/src/main/java/com/ly/mvc_recovery_evaluation/Application.java
@@ -0,0 +1,15 @@
+package com.ly.mvc_recovery_evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuyue
+ * @date 2022/4/7 14:31
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplication(Application.class).run(args);
+    }
+}
