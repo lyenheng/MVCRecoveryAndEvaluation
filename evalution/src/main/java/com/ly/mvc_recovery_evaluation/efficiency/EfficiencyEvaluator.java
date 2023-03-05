dc8498f1dadaf0e4eff156b5521e603ae25b84d3
lyenheng <18839787557@163.com>
2023-03-05 21:36:34 +0800

    评估

---
+++ evalution/src/main/java/com/ly/mvc_recovery_evaluation/efficiency/EfficiencyEvaluator.java
@@ -0,0 +1,19 @@
+package com.ly.mvc_recovery_evaluation.efficiency;
+
+import com.ly.mvc_recovery_evaluation.entity.ClassDescription;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liuyue
+ * @date 2023/3/5 21:20
+ */
+@Service
+public class EfficiencyEvaluator {
+    /**
+     * 模块化评估
+     * @param classDescriptions
+     */
+    public void evaluateModule(ClassDescription[] classDescriptions) {
+
+    }
+}
---
+++ evalution/src/main/java/com/ly/mvc_recovery_evaluation/functionality/FuncationalityEvaluator.java
@@ -0,0 +1,11 @@
+package com.ly.mvc_recovery_evaluation.functionality;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liuyue
+ * @date 2023/3/5 21:21
+ */
+@Service
+public class FuncationalityEvaluator {
+}
